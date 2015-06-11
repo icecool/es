4d4d81dd28aa0e5d6822a3e31976dbaad4427ab8
Komil <komil@code.tj>
2015-06-11 21:40:17 +0500

    update regform add form data and get tracking id

--- app/mvc/m/reg_m.php
+++ app/mvc/m/reg_m.php
@@ -63,58 +63,69 @@ class REG_M {
     		'nom'=>'',
     		'nasab'=>'',
     		'nomi_padar'=>'',
-    		'zodruz'=>'',
+    		'zodruz'=>'0000-00-00',
     		'shahodatnoma'=>'',
-    		'yatim'=>'',
     		'nomi_volidon'=>'',
     		'address'=>'',
     		'email'=>'',
     		'phone'=>'',
     	);
+    	$day=0; $month=0; $year=0;
     	if(isset($_POST['muassisa'])) $val['muassisa']=(int) $_POST['muassisa'];
     	if(isset($_POST['nom'])) $val['nom']=trim($_POST['nom']);
     	if(isset($_POST['nasab'])) $val['nasab']=trim($_POST['nasab']);
     	if(isset($_POST['nomi_padar'])) $val['nomi_padar']=trim($_POST['nomi_padar']);
-    	if(isset($_POST['zodruz'])) $val['zodruzv']=trim($_POST['zodruz']);
-    	if(isset($_POST['shahodatnoma'])) $val['shahodatnoma']=trim($_POST['shahodatnomazodruz']);
 
+    	if(isset($_POST['day'])) $day=(int) $_POST['day'];
+    	if(isset($_POST['month'])) $month=(int) $_POST['month'];
+    	if(isset($_POST['year'])) $year=(int) $_POST['year'];
+
+    	$val['zodruz']=date('Y-m-d',strtotime($year.'-'.$month.'-'.$day));
+
+    	if(isset($_POST['shahodatnoma'])) $val['shahodatnoma']=trim($_POST['shahodatnoma']);
+    	if(isset($_POST['nomi_volidon'])) $val['nomi_volidon']=trim($_POST['nomi_volidon']);
     	if(isset($_POST['address'])) $val['address']=trim($_POST['address']);
+    	if(isset($_POST['email'])) $val['email']=trim($_POST['email']);
     	if(isset($_POST['phone'])) $val['phone']=trim($_POST['phone']);
-    	if(isset($_POST['cellphone'])) $val['cellphone']=trim($_POST['cellphone']);
     	// here should be some data validation
-    	if($val['name_ru']=='') $valid=false; // хотя бы название должно быть указано =)
+    	if($val['nom']=='') $valid=false;
+    	if($val['nasab']=='') $valid=false;
+    	if($val['nomi_padar']=='') $valid=false;
 		if($valid){
 			$DB=\DB::init();
 			if($DB->connected()){
-				$sql = "SELECT * FROM `muassisaho` WHERE `namud`=:namud AND `name_ru`=:name_ru;";
+				$sql = "SELECT * FROM `registration_form` 
+				WHERE `reg-muassisa`=:muassisa AND `reg-nom`=:nom AND `reg-nasab`=:nasab AND `shahodatnoma`=:shahodatnoma;";
 				$sth = $DB->dbh->prepare($sql);
-				$sth->execute(array('namud'=>$val['namud'],'name_ru'=>$val['name_ru']));
-				if($sth->rowCount()>0){
-					echo 'Похоже, что такая запись уже существует в БД';
-					// or system message
+				$sth->execute(array('muassisa'=>$val['muassisa'],'nom'=>$val['nom'],'nasab'=>$val['nasab'],'shahodatnoma'=>$val['shahodatnoma']));
+				if($sth->rowCount()>0 && false){
+					\CORE::init()->msg('error','Похоже, что такая запись уже существует в БД');
 				} else {
-					// add new facility here
 					foreach($val as $k => $v){
 						if($v=='') $val[$k]=NULL;
 					}
-					$sql = "INSERT INTO `muassisaho` SET `namud`=:namud,
-					`name_ru`=:name_ru,
-					`name_tj`=:name_tj,
-					`director`=:director,
-					`address`=:address,
-					`phone`=:phone,
-					`cellphone`=:cellphone
+					$uniqid=uniqid();
+					$sql = "INSERT INTO `registration_form` SET `reg-muassisa`=:muassisa,
+					`reg-nom`=:nom,
+					`reg-nasab`=:nasab,
+					`reg-nomi_padar`=:nomi_padar,
+					`zodruz`=:zodruz,
+					`shahodatnoma`=:shahodatnoma,
+					`nomi_volidon`=:nomi_volidon,
+					`reg-address`=:address,
+					`reg-email`=:email,
+					`reg-phone`=:phone,
+					`reg-code`='".$uniqid."'
 					";
 					$sth = $DB->dbh->prepare($sql);
 					$sth->execute($val);
-					echo 'ok';
+					$GLOBALS['uniqid']=$uniqid;
+					$GLOBALS['didreg']=true;
+					//\CORE::init()->msg('info','Ваша заявка принята на обработку');
 				}
-			} else {
-				echo 'db connection err';
 			}
 		} else {
-			// \CORE::init()->msg('error','Проверьте корректность введенных данных');
-			echo 'Проверьте корректность введенных данных';
+			\CORE::init()->msg('error','Проверьте корректность введенных данных');
 		}
     }
 
