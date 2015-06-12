2a45a1aaff33810736baeee63bc5a544f2b7f64b
Komil <komil@code.tj>
2015-06-12 08:59:30 +0500

    changing frontpage 1

--- app/pages/home_ru.php
+++ app/pages/home_ru.php
@@ -5,24 +5,27 @@ $map_v=new \APP\MVC\V\MAP_V;
 $dv_m=new \APP\MVC\M\DV_M;
 $dv_v=new \APP\MVC\V\DV_V;
 
-$this->pos['main']='
+$this->pos['main']='<br>
 <div class="col-md-4" style="text-align: center">
-            <img src="'.UIPATH.'/img/reg_img.300.png"/>
+            <a href="./?c=reg"><img border="0" src="'.UIPATH.'/img/reg_img.300.png"/></a>
           </div>
           <div class="col-md-4" style="text-align: center">
-            <img src="'.UIPATH.'/img/map_img.300.png"/>
+            <a href="./?c=map"><img border="0" src="'.UIPATH.'/img/map_img.300.png"/></a>
           </div>
           <div class="col-md-4" style="text-align: center">
-            <img src="'.UIPATH.'/img/od_img.300.png"/>
+            <a href="./?c=od"><img border="0" src="'.UIPATH.'/img/od_img.300.png"/></a>
          </div>
 <p>&nbsp;</p>
 <br><br>
 <div class="row">
 <div class="col-md-12">
-<h4 style="text-align: center; font-weight: bold;">Карта покрытыя образовательных учреждений</h4>'.$map_v->main($map_m,false).'
+<h3 class="text-center form_sep_blue"><strong>Карта покрытыя образовательных учреждений</strong></h3>
+'.$map_v->main($map_m,false).'
 </div>
 </div>
 <br><br>
+<h3 class="text-center form_sep_red"><strong>Визуализация данных</strong></h3>
+<br>
 <div class="row">
 <div class="col-md-6">'.$dv_v->bar2($dv_m).'</div>
 <div class="col-md-6">-</div>
