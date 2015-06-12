c19375bdbf5049103fe44aae6cebf658195c7c9a
Bakhtiyor <bakhtiyor.bahriddinov@gmail.com>
2015-06-13 00:11:52 +0600

    Добавил таджикский перевод и таджикские логотипи

--- app/pages/home_tj.php
+++ app/pages/home_tj.php
@@ -1,24 +1,33 @@
 <?php
-$this->pos['main']='<div class="col-md-4">
-            <h2>Introduction</h2>
-            <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, 
-            ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. 
-            Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+$map_m=new \APP\MVC\M\MAP_M;
+$map_v=new \APP\MVC\V\MAP_V;
+
+$dv_m=new \APP\MVC\M\DV_M;
+$dv_v=new \APP\MVC\V\DV_V;
+
+$this->pos['main']='<br>
+<div class="col-md-4" style="text-align: center">
+            <a href="./?c=reg"><img border="0" src="'.UIPATH.'/img/reg_img.300.png"/></a>
           </div>
-          <div class="col-md-4">
-            <h2>Collaboration</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. 
-            Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, 
-            tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <div class="col-md-4" style="text-align: center">
+            <a href="./?c=map"><img border="0" src="'.UIPATH.'/img/map_img.300.png"/></a>
           </div>
-          <div class="col-md-4">
-            <h2>Tools</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, 
-            tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. 
-            Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-         </div> 
-          
+          <div class="col-md-4" style="text-align: center">
+            <a href="./?c=od"><img border="0" src="'.UIPATH.'/img/od_img.300.png"/></a>
+         </div>
+<p>&nbsp;</p>
+<br><br>
+<div class="row">
+<div class="col-md-12">
+<h3 class="text-center form_sep_blue"><strong>'.lang("karta_pokrytiya","Карта покрытия образовательных учреждений г. Душанбе").'</strong></h3>
+'.$map_v->main($map_m,false).'
+</div>
+</div>
+<br><br>
+<h3 class="text-center form_sep_red"><strong>'.lang("dv","Визуализация данных").'</strong></h3>
+<br>
+<div class="row">
+<div class="col-md-6">'.$dv_v->lines($dv_m,1).'</div>
+<div class="col-md-6">'.$dv_v->dbar($dv_m,1).'</div>
+</div>
 ';
