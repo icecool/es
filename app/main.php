e395264816fde71ee2013587d547ee8f5f58fab3
Komil <komil@code.tj>
2015-06-12 09:22:47 +0500

    frontpage changes 2

--- app/main.php
+++ app/main.php
@@ -18,16 +18,22 @@ if($USER->auth()){
 		<!--<li><a href="#about">About</a></li>-->';
 		*/
 	}
-	$UI->pos['user2'].='<li class="dropdown">
+	$UI->pos['user2'].='
+	<li><a href="./?c=muassisaho">'.lang('facilities','Facilities').'</a></li>
+	<li><a href="./?c=reg">'.lang('registration','Регистрация').'</a></li>
+    <li><a href="./?c=map">'.lang('map','Карта').'</a></li>
+    <li><a href="./?c=od">'.lang('od','Открытые данные').'</a></li>
+    <li><a href="./?c=dv">'.lang('dv','Визуализации данных').'</a></li>
+
+
+    <li class="dropdown">
 	  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="true">
-	  '.lang('modules','Modules').'
+	  '.lang('registration','Регистрация').'
 	  <span class="caret"></span></a>
 	  <ul class="dropdown-menu" role="menu">
-	    <li><a href="./?c=muassisaho">'.lang('facilities','Facilities').'</a></li>
-	    <li><a href="./?c=map">'.lang('map','Карта').'</a></li>
-	    <li><a href="./?c=reg">'.lang('registration','Регистрация').'</a></li>
-	    <li><a href="./?c=od">'.lang('od','Открытые данные').'</a></li>
-	    <li><a href="./?c=dv">'.lang('dv','Визуализации данных').'</a></li>	    
+	    li><a href="./?c=reg">'.lang('regform','Форма регистрации').'</a></li>
+	    <li><a href="./?c=reg&act=check">'.lang('checkreg','Проверить статус заявки').'</a></li>
 	  </ul>
-	</li>';
+	</li>
+	';
 }
--- ui/tpl/es/tpl.php
+++ ui/tpl/es/tpl.php
@@ -32,7 +32,7 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="./">
-          <?php echo lang('es','Education Services'); ?></a>
+          <strong><?php echo lang('openedu','OpenEDU'); ?></strong></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
